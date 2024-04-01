46bc5fed3692038925aa27b1fd7020999c35915b
Mohamed Diab <81900099+mokdiab@users.noreply.github.com>
2024-04-01 22:21:33 +0200

    add: deploy workflow

--- src/compmnents/ResultsData.jsx
+++ src/compmnents/ResultsData.jsx
@@ -7,34 +7,39 @@ export default function ResultsData({ userData }) {
     annualData[0].annualInvestment -
     annualData[0].interest;
   return (
-    <table id="result">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>INVESTMENT VALUE</th>
-          <th>INTEREST (YEAR)</th>
-          <th>TOTAL INTEREST</th>
-          <th>INVESTED CAPITAL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {annualData.map((data, i) => {
-          const totalInterest =
-            data.valueEndOfYear -
-            data.annualInvestment * data.year -
-            initialValue;
-          const totalCapital = data.valueEndOfYear - totalInterest;
-          return (
-            <tr key={i}>
-              <th>{data.year}</th>
-              <th>{formatter.format(data.valueEndOfYear)}</th>
-              <th>{formatter.format(data.interest)}</th>
-              <th>{formatter.format(totalInterest)}</th>
-              <th>{formatter.format(totalCapital)}</th>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>INVESTMENT VALUE</th>
+            <th>INTEREST (YEAR)</th>
+            <th>TOTAL INTEREST</th>
+            <th>INVESTED CAPITAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {annualData.map((data, i) => {
+            const totalInterest =
+              data.valueEndOfYear -
+              data.annualInvestment * data.year -
+              initialValue;
+            const totalCapital = data.valueEndOfYear - totalInterest;
+            return (
+              <tr key={i}>
+                <th>{data.year}</th>
+                <th>{formatter.format(data.valueEndOfYear)}</th>
+                <th>{formatter.format(data.interest)}</th>
+                <th>{formatter.format(totalInterest)}</th>
+                <th>{formatter.format(totalCapital)}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <p className="footer-text">Mohamed Diab</p>
+      </div>
+    </>
   );
 }
